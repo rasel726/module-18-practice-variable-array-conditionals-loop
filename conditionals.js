2065cbe0382b2ecce878644a347390fb02a17cdd
rasel726 <md.rasel2726@gmail.com>
2022-08-06 15:06:11 +0600

    conditionals file added

---
+++ conditionals.js
@@ -0,0 +1,14 @@
+/**
+ * ----------------
+ * CONDITIONALS
+ * ----------------
+ * 1. Meaning of: >, <, >=, <=, ==, !=, ===, !==
+ * 2. Meaning of &&
+ * 3. Meaning of ||
+ * 4. Let's say you have x amount of money. If you have more than 80,000, then you will buy a mac
+ *      if more than 60,000 then you will buy gaming laptop
+ *      if you more than 40,000 then you will buy Leveno Yoga
+ *      if you more than 20,000 then you will buy the use laptop
+ *      otherwise, you will use you mobile phone
+ * 
+ */
